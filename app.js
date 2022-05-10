49e8f8d403d38f9e857fce734151faaf81b44348
huymai0107 <80440582+huymai0107@users.noreply.github.com>
2022-05-11 01:57:43 +0700

    connect with db

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const exphbs = require("express-handlebars");
 const bodyParser = require("body-parser");
 const mysql = require("mysql");
+const Connection = require("mysql/lib/Connection");
 
 require("dotenv").config();
 
@@ -21,11 +22,32 @@ app.use(express.static("public"));
 app.engine("hbs", exphbs.engine({ extname: ".hbs" }));
 app.set("view engine", "hbs");
 
+
+
+
+
+const pool = mysql.createPool({
+  connectionLimit : 100, //important
+  host     : 'localhost',
+  user     : 'root',
+  password : 'Bestteemo',
+  database : 'Video_Streaming',
+  debug    :  false
+});
+
+pool.getConnection((err, connection) => {
+  if(err) throw err;
+  console.log('Connected as ID ' + connection.threadId);
+})
+
 //Router to render the page
 app.get("", (req, res) => {
   res.render("home.hbs");
   //   res.render("movies.hbs");
   //   res.render("series.hbs");
 });
+
+
+
 
 app.listen(port, () => console.log("Listening on port " + port));
