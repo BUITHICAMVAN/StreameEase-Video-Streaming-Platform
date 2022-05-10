6d4994ac0db5c7d121e871bea52305bf3b393f0c
BUI CAM VAN <81848643+BUITHICAMVAN@users.noreply.github.com>
2022-05-10 14:51:01 +0700

    add movies and series

--- app.js
+++ app.js
@@ -1,33 +1,30 @@
-const express = require('express');
-const exphbs = require('express-handlebars');
-const bodyParser = require('body-parser')
-const mysql = require('mysql')
+const express = require("express");
+const exphbs = require("express-handlebars");
+const bodyParser = require("body-parser");
+const mysql = require("mysql");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 2000;
 
-
-
 //Parsing middleware
 //Parse application/ x-ww-form-urlencoded
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Parse appllication/json
 app.use(bodyParser.json());
 //static files
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 //templation engine
-app.engine('hbs', exphbs.engine( {extname: '.hbs'}))
-app.set('view engine', 'hbs');
+app.engine("hbs", exphbs.engine({ extname: ".hbs" }));
+app.set("view engine", "hbs");
 
 //Router to render the page
-app.get('', (req, res) => {
-    res.render('home.hbs');
+app.get("", (req, res) => {
+  res.render("home.hbs");
+  res.render("movies.hbs");
 });
 
-
-
-app.listen(port, () => console.log('Listening on port ' + port))
+app.listen(port, () => console.log("Listening on port " + port));
