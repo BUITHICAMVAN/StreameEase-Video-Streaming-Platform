47487a8d4ec6a6f1db0948044306099afc79607c
huymai0107 <80440582+huymai0107@users.noreply.github.com>
2022-05-10 13:30:09 +0700

    First commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser')
+const mysql = require('mysql')
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 2000;
+
+
+
+//Parsing middleware
+//Parse application/ x-ww-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Parse appllication/json
+app.use(bodyParser.json());
+//static files
+app.use(express.static('public'));
+
+//templation engine
+app.engine('hbs', exphbs.engine( {extname: '.hbs'}))
+app.set('view engine', 'hbs');
+
+//Router
+app.get('', (req, res) => {
+    res.render('home.hbs');
+})
+
+
+
+app.listen(port, () => console.log('Listening on port ' + port))
