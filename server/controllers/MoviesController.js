bcee744ab244e9921ab3222104c7591d9166d724
BUI CAM VAN <81848643+BUITHICAMVAN@users.noreply.github.com>
2022-05-11 17:09:23 +0700

    Add embedded videos

--- server/controllers/MoviesController.js
+++ server/controllers/MoviesController.js
@@ -1,31 +1,26 @@
-const res = require('express/lib/response');
-const mysql = require('mysql')
+const res = require("express/lib/response");
+const mysql = require("mysql");
 
 const pool = mysql.createPool({
-    connectionLimit : 100, //important
-    host     : 'localhost',
-    database : 'Video_Streaming',
-    user     : 'root',
-    password : 'Bestteemo',
-  });
+  connectionLimit: 100, //important
+  host: "localhost",
+  database: "video_streaming",
+  user: "root",
+  password: "@@@Btcv08122002",
+});
 // Views Movie
-  exports.view = (req, res) =>
-{
-    pool.getConnection((err, connection) => {
-        if(err) throw err;
-        console.log('Connected as ID ' + connection.threadId);
-        connection.query('select * from Movies', (err, rows) =>{
-            connection.release();
-            if(!err)
-            res.render('home', {rows});
-            else{
-                console.log(err)
-            }
-    
-            console.log(rows);
-        })
-    })
-}
- 
+exports.view = (req, res) => {
+  pool.getConnection((err, connection) => {
+    if (err) throw err;
+    console.log("Connected as ID " + connection.threadId);
+    connection.query("select * from Movies", (err, rows) => {
+      connection.release();
+      if (!err) res.render("home", { rows });
+      else {
+        console.log(err);
+      }
 
-
+      console.log(rows);
+    });
+  });
+};
