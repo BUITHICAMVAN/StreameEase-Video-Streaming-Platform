1e35900325ceb237cfe5a0786cfdc9228ccc3e37
BUI CAM VAN <81848643+BUITHICAMVAN@users.noreply.github.com>
2022-05-12 18:22:23 +0700

    update search

--- server/controllers/MoviesController.js
+++ server/controllers/MoviesController.js
@@ -8,19 +8,41 @@ const pool = mysql.createPool({
   user: "root",
   password: "@@@Btcv08122002",
 });
+
 // Views Movie
 exports.view = (req, res) => {
   pool.getConnection((err, connection) => {
     if (err) throw err;
     console.log("Connected as ID " + connection.threadId);
-    connection.query("select * from Movies", (err, rows) => {
+    connection.query("SELECT * FROM Movies", (err, rows) => {
+      //when done with connection
       connection.release();
+
+      // if there is no err, render page home
       if (!err) res.render("home", { rows });
       else {
         console.log(err);
       }
 
-      console.log(rows);
+      console.log("The data from database: \n", rows);
     });
   });
+};
+
+//Find user by search
+exports.find = (req, res) => {
+  let searchTerm = req.body.search;
+  // User the connection
+  connection.query(
+    "SELECT * FROM user WHERE Tiltle LIKE ? OR idMovie LIKE ?",
+    ["%" + searchTerm + "%", "%" + searchTerm + "%"],
+    (err, rows) => {
+      if (!err) {
+        res.render("home", { rows });
+      } else {
+        console.log(err);
+      }
+      console.log("The data from user table: \n", rows);
+    }
+  );
 };
