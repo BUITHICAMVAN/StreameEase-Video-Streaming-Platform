38f3e5ee2616b123a548f6a1184d7fdf52bf98f0
huymai0107 <80440582+huymai0107@users.noreply.github.com>
2022-05-14 00:14:02 +0700

    add successfully add alert

--- server/controllers/MoviesController.js
+++ server/controllers/MoviesController.js
@@ -49,17 +49,34 @@ exports.find = (req, res) => {
     res.render('add-movies');
   };
 
+  // exports.create = (req, res) => {
+  //   const {idMovie,Title,Year,Genre,Director,Play} = req.body;
+  //   // User the connection
+  //   pool.getConnection((err, connection) => {
+  //     connection.query('insert into Movies set idMovie = ?,Title =  ?,Year = ?, Genre = ?, Director = ?, Play = ?', [idMovie,Title,Year,Genre,Director,Play], (err, rows) => {
+  //     connection.release();
+  //     if (!err) {
+  //       res.render('add-movies', { alert: 'User added successfully.' });
+  //     } else {
+  //       console.log(err);
+  //     }
+  //     console.log('successfully inserted');
+  //   });})
+  // };  
+
   exports.create = (req, res) => {
     const {idMovie,Title,Year,Genre,Director,Play} = req.body;
+    let searchTerm = req.body.search;
+  
     // User the connection
-    pool.getConnection((err, connection) => {
-      connection.query('insert into Movies set idMovie = ?,Title =  ?,Year = ?, Genre = ?, Director = ?, Play = ?', [idMovie,Title,Year,Genre,Director,Play], (err, rows) => {
-      connection.release();
+        pool.getConnection((err, connection) => {
+    connection.query('insert into Movies set idMovie = ?,Title =  ?,Year = ?, Genre = ?, Director = ?, Play = ?', [idMovie,Title,Year,Genre,Director,Play], (err, rows) => {
       if (!err) {
-        res.render('add-movies');
+        res.render('add-movies', { alert: 'Added successfully.' });
       } else {
         console.log(err);
       }
-      console.log('successfully inserted');
+      console.log('The data from user table: \n', rows);
     });})
-  };  
+}
+  
